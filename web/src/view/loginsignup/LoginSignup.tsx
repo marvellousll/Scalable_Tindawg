1181462769e11cc9e8b51618cc7966c7973b56e0
Yining Wang <36571521+kyswn@users.noreply.github.com>
2020-10-31 15:00:01 -0700

    finished login page

--- web/src/view/loginsignup/LoginSignup.tsx
+++ web/src/view/loginsignup/LoginSignup.tsx
@@ -1,14 +1,46 @@
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
+import GoogleLogin from 'react-google-login'
 import { AppRouteParams } from '../nav/route'
-//import * as dog from './dog.jpg'
-//const dog = require('./dog.jpg');
+
+const responseGoogle = (response: any) => {
+  console.log(response)
+}
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}))
+//'https://www.rockymtnresorts.com/wp-content/uploads/2018/12/dog-friendly01.jpg'
 interface LoginSignupProps extends RouteComponentProps, AppRouteParams {}
 export const LoginSignup = (props: LoginSignupProps) => {
-  //return <img src={dog} />
+  const myStyle = {
+    backgroundImage: 'url(https://www.rockymtnresorts.com/wp-content/uploads/2018/12/dog-friendly01.jpg)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    weight: '100vh',
+  }
+  const classes = useStyles()
   return (
-    <div>
-      <img src="https://www.rockymtnresorts.com/wp-content/uploads/2018/12/dog-friendly01.jpg" alt="new"></img>
-    </div>
+    <Container fixed>
+      <div style={myStyle}>
+        <div style={{ color: 'black', fontSize: '60' }}>TINDAWG</div>
+        <div className={classes.root}>
+          <GoogleLogin
+            clientId="24410381832-jg15gpvg63pt3bj7hjmeevft9i2usit9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </div>
+      </div>
+    </Container>
   )
 }

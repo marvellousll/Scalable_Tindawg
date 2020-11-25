7adc96d80340155369a74c8f1b07767c968624fd
Yuan Cheng <ycheng97@ucla.edu>
2020-11-24 22:26:46 -0800

    minor bug fix

---
+++ server/src/db/migrations/V1.1__Add_user.sql
@@ -0,0 +1,1 @@
+insert into `user` (`id`, `email`, `userType`, `password`) values ('1', 'test@test.com', 'USER', 'test');
--- web/src/view/page/LoginPage.tsx
+++ web/src/view/page/LoginPage.tsx
@@ -1,13 +1,28 @@
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
+import { useContext } from 'react'
 import { LoginStyle } from '../../style/login'
+import { PageStyle } from '../../style/page'
 import { Login } from '../auth/Login'
+import { UserContext } from '../auth/user'
+import { NavBar } from '../nav/NavBar'
 import { AppRouteParams } from '../nav/route'
 
 interface LoginPageProps extends RouteComponentProps, AppRouteParams {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function LoginPage(props: LoginPageProps) {
+  const { user } = useContext(UserContext)
+
+  if (user)
+    return (
+      <div className="mw8" style={PageStyle}>
+        <NavBar />
+        <div style={LoginStyle}>
+          <Login />
+        </div>
+      </div>
+    )
   return (
     <div style={LoginStyle}>
       <Login />
